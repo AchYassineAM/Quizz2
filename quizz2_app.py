52fc42370416f79178b8e43ef5dfacd5de53696a
AchYassineAM <163415750+AchYassineAM@users.noreply.github.com>
2024-03-15 12:49:59 +0100

    Update and rename quizz_app.py to quizz2_app.py

--- quizz2_app.py
+++ quizz2_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import openpyxl
 
 # Charger le fichier Excel contenant les 99 questions et réponses
 file_path = "99Questions.xlsx"
-df = pd.read_excel("99Questions.xlsx")
+df = pd.read_excel(file_path)
 
 # Titre de l'application
 st.title("Quiz App")
@@ -35,7 +33,11 @@ if st.button("Générer"):
     final_selected_questions = pd.concat([included_df, random_questions])
 
     # Afficher le tableau résultant
-    st.write(final_selected_questions[['Numéro', 'Question', 'Réponse']])
+    for index, row in final_selected_questions.iterrows():
+        st.write(f"Question {row['Numéro']}: {row['Question']}")
+        st.write(f"Réponse {row['Numéro']}: {row['Réponse']}")
 else:
     # Afficher le tableau complet si le bouton n'a pas encore été cliqué
-    st.write(df[['Numéro', 'Question', 'Réponse']])
+    for index, row in df.iterrows():
+        st.write(f"Question {row['Numéro']}: {row['Question']}")
+        st.write(f"Réponse {row['Numéro']}: {row['Réponse']}")
